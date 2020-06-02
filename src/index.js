96955765a1e6535e002050b3a2d716e6029aa90d
yuli-ferna <yuliferna123@gmail.com>
2020-06-02 12:22:16 -0400

    YT and twitch playes with url

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ import { Vector3, BufferGeometryLoader } from 'three';
 
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 
+const MATCH_URL_TWITCH_VIDEO = /(?:www\.|go\.)?twitch\.tv\/videos\/(\d+)($|\?)/
+const MATCH_URL_TWITCH_CHANNEL = /(?:www\.|go\.)?twitch\.tv\/([a-zA-Z0-9_]+)($|\?)/
 const MATCH_URL_YOUTUBE = /(?:youtu\.be\/|youtube(?:-nocookie)?\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})|youtube\.com\/playlist\?list=|youtube\.com\/user\//
 let config ={};
 let rendererCSS, sceneCSS, camera;
@@ -33,8 +35,75 @@ let direction = new THREE.Vector3();
 let mouse = new THREE.Vector2();
 
 let controls;
+let addPopupURL = ( id, idIFrame, player, pos, ry = 0 ) =>{
+	let div = document.createElement('div');
+	div.style.fontFamily = "'Roboto', sans-serif";
+	div.id = id + '-container';
 
-let addInput = ( id, pos, ry = 0, width = 480, height = 80 ) => {
+	let button = document.createElement('button');
+	button.style.fontSize = '20px';
+	button.style.borderRadius = '10px';
+	button.style.padding = '5px';
+	button.style.border = 'none';
+	button.style.background = '#81f1ff';
+	button.innerHTML = 'Insert URL';
+
+	button.addEventListener('click', function (params) {
+		let url = prompt("Please enter " + player + " URL:", "");
+		let cont = document.getElementById( id + '-container' );
+		if (url == null || url == "") {
+			// let war = document.createElement('p');
+			// war.innerHTML = "Insert valid youtube url";
+			// war.id = 'war';
+			// cont.appendChild( war );
+			console.log('WORK');
+			
+		} else 
+		{
+			if (document.getElementById('error')) 
+			{
+				document.getElementById('error').remove();
+			}
+			console.log(url);
+			
+			let embedURL;
+			switch (player) {
+				case 'youtube':
+					embedURL = getEmbedYT(url);
+					
+					break;
+				case 'twitch':
+					embedURL = getEmbedTwitch(url);
+					
+					break;
+				
+				default:
+					embedURL = null;
+					break;
+			}
+			if (embedURL) {
+				let iframeVideo = document.getElementById( idIFrame );
+				iframeVideo.src = embedURL;
+			}else{
+				// let error = document.createElement('p');
+				// error.innerHTML = "Insert valid " + player + " url";
+				// error.style.color = 'red';
+				// error.id = 'error';
+				// cont.appendChild( error );
+				alert("Insert valid " + player + " url")
+			}
+		}
+		
+	})
+
+	div.appendChild( button );
+	let object = new CSS3DObject( div );
+	object.position.set( pos.x, pos.y, pos.z );
+	object.rotation.y = ry;
+
+	config.sceneCSS.add(object);
+}
+let addInput = ( id, pos, ry = 0, width = 480, height = 40 ) => {
 	let div = document.createElement( 'div' );
 	div.style.width = width + 'px';
 	div.style.height = height + 'px';
@@ -71,17 +140,16 @@ let addInput = ( id, pos, ry = 0, width = 480, height = 80 ) => {
 		console.log(url);
 		
 		let cont = document.getElementById(id + '-container');
-		let embedURL = getEmbedYT(url);
+		let embedURL = id == 'youtubeURL' ? getEmbedYT(url) : getEmbedTwitch(url);
 		if (embedURL) {
-			let iframeYT = document.getElementById('youtube');
-			iframeYT.src = embedURL;
+			let iframeVideo = document.getElementById(id);
+			iframeVideo.src = embedURL;
 		}else{
 			let error = document.createElement('p');
 			error.innerHTML = "Insert valid youtube url";
 			error.style.color = 'red';
 			error.id = 'error';
 			cont.appendChild( error );
-
 		}
 		
 	})
@@ -97,7 +165,7 @@ let addInput = ( id, pos, ry = 0, width = 480, height = 80 ) => {
 	config.sceneCSS.add(object);
 };
 
-let addIFrame = ( src, id, pos, ry, width = 480, height = 360 ) => {
+let addIFrame = ( src, id, pos, ry, width = 1080, height = 620 ) => {
 
 	let div = document.createElement( 'div' );
 	div.style.width = width + 'px';
@@ -151,99 +219,25 @@ let getEmbedYT = (url) => {
 	}
 }
 
-var player;
-var onKeyDown = function ( event ) {
-
-	switch ( event.keyCode ) {
-
-		case 38: // up
-		case 87: // w
-			moveForward = true;
-			break;
-
-		case 37: // left
-		case 65: // a
-			moveLeft = true;
-			break;
-
-		case 40: // down
-		case 83: // s
-			moveBackward = true;
-			break;
-
-		case 39: // right
-		case 68: // d
-			moveRight = true;
-			break;
-
-		case 32: // space
-			if ( canJump === true ) velocity.y += 350;
-			canJump = false;
-			break;
-
+let getEmbedTwitch = (url) => {
+	//https://player.twitch.tv/?<channel, video, or collection>&parent=streamernews.example.com
+	const isChannel = MATCH_URL_TWITCH_CHANNEL.test(url)
+	const isVideo = MATCH_URL_TWITCH_VIDEO.test(url)
+	if (isVideo) {
+		let id = url.match(MATCH_URL_TWITCH_VIDEO)[1]
+		let embedURL = 'https://player.twitch.tv/?video=' + id ;
+		return embedURL;
 	}
-
-};
-
-var onKeyUp = function ( event ) {
-
-	switch ( event.keyCode ) {
-
-		case 38: // up
-		case 87: // w
-			moveForward = false;
-			break;
-
-		case 37: // left
-		case 65: // a
-			moveLeft = false;
-			break;
-
-		case 40: // down
-		case 83: // s
-			moveBackward = false;
-			break;
-
-		case 39: // right
-		case 68: // d
-			moveRight = false;
-			break;
-
+	else if (isChannel) {
+		let id = url.match(MATCH_URL_TWITCH_CHANNEL)[1]
+		let embedURL = 'https://player.twitch.tv/?channel=' + id ;
+		return embedURL;
 	}
-
-};
-document.addEventListener( 'keydown', onKeyDown, false );
-document.addEventListener( 'keyup', onKeyUp, false );
-
-function raycast(e) {
-	
-    //1. sets the mouse position with a coordinate system where the center
-    //   of the screen is the origin
-    mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-
-    //2. set the picking ray from the camera position and mouse coordinates
-    raycaster.setFromCamera( mouse, config.camera );    
-	console.log('hey');
-	
-    //3. compute intersections
-    var intersects = raycaster.intersectObjects( config.sceneGL.children );
-	console.log(intersects);
-
-    for ( var i = 0; i < intersects.length; i++ ) {
-        console.log( intersects[ i ] ); 
-        /*
-            An intersection has the following properties :
-                - object : intersected object (THREE.Mesh)
-                - distance : distance from camera to intersection (number)
-                - face : intersected face (THREE.Face3)
-                - faceIndex : intersected face index (number)
-                - point : intersection point (THREE.Vector3)
-                - uv : intersection point in the object's UV coordinates (THREE.Vector2)
-        */
-    }
-
+	else{
+		return null;
+	}
 }
+
 
 let init = () => {
 	
@@ -261,11 +255,7 @@ let init = () => {
 	config.rendererCSS.domElement.style.position = 'absolute';
     config.rendererCSS.domElement.style.top = 0;
 	document.getElementById('css3d').appendChild(config.rendererCSS.domElement);
-	// width: 100%;
-	// height: 100%;
-	// position: absolute;
-	// top: 0; left: 0;
-
+	
 	// create a WebGLRenderer
 	config.rendererGL = new THREE.WebGLRenderer({ alpha: true, antialias: true });
     config.rendererGL.setClearColor( 0x000000, 0 );
@@ -275,14 +265,6 @@ let init = () => {
 	config.rendererGL.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 	config.rendererGL.domElement.style.zIndex = "1";
 	document.getElementById('gl').appendChild(config.rendererGL.domElement);
-	// width: 100%;
-	// height: 100%;
-	// position: absolute;
-	// top: 0; left: 0;
-	// pointer-events:none
-
-	raycaster = new THREE.Raycaster();
-    config.rendererGL.domElement.addEventListener( 'click', raycast, false );
 
 
 	// position and point the config.camera to the center of the scene
@@ -291,15 +273,16 @@ let init = () => {
 	config.camera.position.z = 1300;
 	// config.camera.up = new THREE.Vector3(0,0,1);
 	// config.camera.lookAt(scene.position);
-	let asd= 'https://www.youtube.com/embed/U5vTlFp4Gew' +'?enablejsapi=1&origin=http://example.com'
-	let url= 'https://www.youtube.com/watch?v=U5vTlFp4Gew';
-	addIFrame( asd, 'youtube', new Vector3( 0, 0, 0 ), 0 );
-	//https://player.twitch.tv/?<channel, video, or collection>&parent=streamernews.example.com
-	addIFrame( 'https://player.twitch.tv/?channel=tfue&parent=streamernews.example.com&autoplay=false', 'twitch', new Vector3( -550, 0, 150 ), 45 * Math.PI / 180 );
-	addIFrame( 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebookapp%2Fvideos%2F10153231379946729%2F&show_text=0', 'facebook',  new Vector3( 550, 0, 150 ), -45 * Math.PI / 180 );
-	// addIFrameYT('https://www.youtube.com/watch?v=U5vTlFp4Gew', new Vector3( 0, 0, 0 ), 0)
 	
-	addInput( 'urlVideo', new Vector3( 0, 250, 0 ), 0 );
+	let url= 'https://www.youtube.com/watch?v=U5vTlFp4Gew';
+	
+	addIFrame( getEmbedYT(url), 'youtube', new Vector3( 550, 200, 0 ), 0 );
+	
+	addIFrame( getEmbedTwitch('https://www.twitch.tv/tfue'), 'twitch', new Vector3( -550, 200, 0 ), 0 );
+	
+	// addInput( 'youtubeURL', new Vector3( 0, 250, 0 ), 0 );
+	addPopupURL( 'youtubeURL', 'youtube', 'youtube', new Vector3( 550, -130, 0 ), 0 );
+	addPopupURL( 'twitchURL', 'twitch', 'twitch', new Vector3( -550, -130, 0 ), 0 );
 	config.controls = new OrbitControls( config.camera, config.rendererCSS.domElement );
 	// config.controlsGL = new OrbitControls( config.camera, config.rendererGL.domElement );
 	// config.controls = new PointerLockControls( camera, document.body );
@@ -316,17 +299,26 @@ let init = () => {
 
 	let light = new THREE.AmbientLight(0xffffff);
 	config.sceneGL.add(light);
-	let pointLight = new THREE.PointLight(0xffffff);
-	pointLight.position.set(0, 300, 100);
-	config.sceneGL.add(pointLight);
-	let helperPointLight = new THREE.PointLightHelper(pointLight);
-	config.sceneGL.add(helperPointLight);
+	// let pointLight = new THREE.PointLight(0xffffff);
+	// pointLight.position.set(0, 300, 100);
+	// config.sceneGL.add(pointLight);
+	// let helperPointLight = new THREE.PointLightHelper(pointLight);
+	// config.sceneGL.add(helperPointLight);
 	let geometry = new THREE.BoxGeometry( 100, 100, 100 );
 	let material = new THREE.MeshStandardMaterial( {color: 0x00ff00} );
 	let cube = new THREE.Mesh( geometry, material );
-	cube.position.set(0,-100, 500);
+	cube.position.set(0,300, 500);
 	config.sceneGL.add( cube );
-
+	let floorTexture = new THREE.ImageUtils.loadTexture( 'assets/checkerboard.jpg' );
+	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
+	floorTexture.repeat.set( 35, 35 );
+	let floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+	let floorGeometry = new THREE.PlaneGeometry(3500, 3500, 35, 35);
+	let floor = new THREE.Mesh(floorGeometry, floorMaterial);
+	floor.position.y = 450;
+	floor.rotation.x = -Math.PI / 3;
+	// floor.rotation.z = Math.PI / 2;
+	config.sceneGL.add(floor);
 }
 
 function displayWindowSize(){
@@ -365,20 +357,20 @@ function animate()
 	// 	config.rendererGL.render( config.sceneGL, config.camera );
 
 	// }
-	if (config.camera.position.z < 2200 && !renderCSS3D) {
-		console.log(config.camera.position);
-		renderCSS3D = true;
-		document.getElementById('css3d').style.display = 'block' ;
-		document.getElementById('gl').style.pointerEvents = 'none';
-		config.rendererGL.setClearColor( 0x000000, 0);
-	} 
-	if(config.camera.position.z >= 2200 && renderCSS3D){
-		console.log(config.camera.position);
-		renderCSS3D = false;
-		// document.getElementById('css3d').style.display =  'none';
-		document.getElementById('gl').style.pointerEvents =  'all';
-		config.rendererGL.setClearColor( 0x000000,.5 );
-	}
+	// if (config.camera.position.z < 2200 && !renderCSS3D) {
+	// 	console.log(config.camera.position);
+	// 	renderCSS3D = true;
+	// 	document.getElementById('css3d').style.display = 'block' ;
+	// 	// document.getElementById('gl').style.pointerEvents = 'none';
+	// 	config.rendererGL.setClearColor( 0x000000, 0);
+	// } 
+	// if(config.camera.position.z >= 2200 && renderCSS3D){
+	// 	console.log(config.camera.position);
+	// 	renderCSS3D = false;
+	// 	// document.getElementById('css3d').style.display =  'none';
+	// 	document.getElementById('gl').style.pointerEvents =  'all';
+	// 	config.rendererGL.setClearColor( 0x000000,.5 );
+	// }
 
 }
 
